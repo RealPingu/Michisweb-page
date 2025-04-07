1f69cd989ef216d9add9423bbe0c363507ae4840
michaelflemingmanz <michaelflemingmanz@gmail.com>
2025-04-07 18:18:01 -0400

    Initial commit

---
+++ src/routes/Router.tsx
@@ -0,0 +1,36 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import { Login } from "../screens/Login/Login";
+
+import { MenuFuncionario } from "../screens/Funcionario/Menu";
+import { MenuMedico } from "../screens/Medico/Menu";
+
+import { Prescripciones } from "../screens/Funcionario/Prescripciones/Prescripciones";
+import { MenuStock } from "../screens/Funcionario/Stock/Menu";
+
+import { IngresarMedicamentos } from "../screens/Funcionario/Stock/IngresarMedicamentos";
+import { RegistrarEntrega } from "../screens/Funcionario/Stock/RegistrarEntrega";
+import { EmitirInformes } from "../screens/Funcionario/Stock/EmitirInformes";
+import { Informes } from "../screens/Funcionario/Stock/Informes/Informes";
+import { BajaMedicamentos } from "../screens/Funcionario/Stock/BajaMedicamentos";
+
+import { Reserva } from "../screens/Funcionario/Prescripciones/Reserva/Reserva"
+
+export const Router = createBrowserRouter([
+  { path: "/", element: <Login /> },
+
+  { path: "/funcionario", element: <MenuFuncionario /> },
+  { path: "/medico", element: <MenuMedico /> },
+
+  { path: "/funcionario/prescripciones", element: <Prescripciones /> },
+  { path: "/funcionario/stock", element: <MenuStock /> },
+
+  { path: "/funcionario/stock/ingresar-medicamentos", element: <IngresarMedicamentos /> },
+  { path: "/funcionario/stock/registrar-entrega", element: <RegistrarEntrega /> },
+  { path: "/funcionario/stock/emitir-informes", element: <EmitirInformes /> },
+  { path: "/funcionario/stock/emitir-informes/informes", element: <Informes /> },
+  { path: "/funcionario/stock/baja-medicamentos", element: <BajaMedicamentos /> },
+
+  { path: "/funcionario/prescripciones/reserva", element: <Reserva /> },
+  
+]);
---
+++ src/screens/Funcionario/Stock/Informes/Informes.tsx
@@ -0,0 +1,8 @@
+export const Informes = () => {
+    return (
+      <div className="p-4">
+        <h2 className="text-xl font-bold mb-2">Informes</h2>
+        <p className="text-gray-700">En construcción</p>
+      </div>
+    );
+  };
