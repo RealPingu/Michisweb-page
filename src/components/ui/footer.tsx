0d96c6d89d71c2cd41abaf5e75ba3cf939eb4316
evwng <eva29wang@gmail.com>
2025-04-14 22:14:53 -0400

    Fix footer

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -5,11 +5,11 @@ export const FooterFuncionarioPrescripciones = () => {
     const navigate = useNavigate();
     return (
       <div className="fixed bottom-0 left-1/2 -translate-x-1/2 w-full max-w-md bg-gray-100 border-t text-center text-sm text-gray-500 py-2 flex justify-around">
-        <div className="flex flex-col items-center hover:text-black cursor-pointer" onClick={() => navigate("/medico/revisar-stock")}>
+        <div className="flex flex-col items-center hover:text-black cursor-pointer" onClick={() => navigate("/funcionario/Prescripciones/reservas")}>
           <Calendar className="w-5 h-5" />
           <span>Reservas</span>
         </div>
-        <div className="flex flex-col items-center hover:text-black cursor-pointer" onClick={() => navigate("/medico/ingresar-prescripcion")}>
+        <div className="flex flex-col items-center hover:text-black cursor-pointer" onClick={() => navigate("/funcionario/prescripciones/pendientes")}>
           <ClipboardList className="w-5 h-5" />
           <span>Prescripciones</span>
         </div>
@@ -21,19 +21,19 @@ export const FooterFuncionarioStock = () => {
     const navigate = useNavigate();
     return (
       <div className="fixed bottom-0 left-1/2 -translate-x-1/2 w-full max-w-md bg-gray-100 border-t text-center text-sm text-gray-500 py-2 flex justify-around">
-        <div className="flex flex-col items-center hover:text-black cursor-pointer" onClick={() => navigate("/medico/revisar-stock")}>
+        <div className="flex flex-col items-center hover:text-black cursor-pointer" onClick={() => navigate("/funcionario/stock/ingresar-medicamentos")}>
           <Package className="w-5 h-5" />
           <span>Stock</span>
         </div>
-        <div className="flex flex-col items-center hover:text-black cursor-pointer" onClick={() => navigate("/medico/ingresar-prescripcion")}>
+        <div className="flex flex-col items-center hover:text-black cursor-pointer" onClick={() => navigate("/funcionario/stock/registrar-entrega")}>
           <Truck className="w-5 h-5" />
           <span>Entrega</span>
         </div>
-        <div className="flex flex-col items-center hover:text-black cursor-pointer" onClick={() => navigate("/medico/emitir-recetas")}>
+        <div className="flex flex-col items-center hover:text-black cursor-pointer" onClick={() => navigate("/funcionario/stock/emitir-informes")}>
           <FileText className="w-5 h-5" />
           <span>Informe</span>
         </div>
-        <div className="flex flex-col items-center hover:text-black cursor-pointer" onClick={() => navigate("/medico/emitir-recetas")}>
+        <div className="flex flex-col items-center hover:text-black cursor-pointer" onClick={() => navigate("/funcionario/stock/baja-medicamentos")}>
           <ArrowDownCircle className="w-5 h-5" />
           <span>Baja</span>
         </div>
